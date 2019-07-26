deb66566cbc974cb4d8c58270c4c3d8d667c1b13
hendy@digitalhouse.com <hendy@digitalhouse.com>
2019-07-26 17:51:45 +0000

    Mumuki Export on 2019-07-26 17:51:45 +0000

--- 00011_A quantidade de estudantes/test.php
+++ 00011_A quantidade de estudantes/test.php
@@ -6,7 +6,7 @@ public function testQuantidadeEstudantes(): void {
         
         foreach ($classe->getProperties() as $p) {
             if ($p->name == "quantidadeEstudantes") {
-                $temQuantidade = true;
+                $quantidadeEstudantes = true;
                 $eEstatica = $p->isStatic();
             }
         }
