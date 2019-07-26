c4a9ef91f2c4b40620c213bb2350e1f4bcc5bc7b
hendy@digitalhouse.com <hendy@digitalhouse.com>
2019-07-26 00:33:30 +0000

    Mumuki Export on 2019-07-26 00:33:30 +0000

--- 00008_Pagando um empregado/test.php
+++ 00008_Pagando um empregado/test.php
@@ -1,15 +1,15 @@
 public function testImplementa(): void {
-        $funcionario = new Funcionario("Jane", "Doe", "victor@dh.com", 500);
+        $funcionario = new Funcionario("Jane", "Doe", "jane@dh.com", 500);
         
         $this->assertTrue($funcionario instanceof Pagavel, "A classe Funcionario não implementa a interface Pagavel");
     }
 
     public function testPagar() : void {
-        $funcionario = new Funcionario("Jane", "Doe", "victor@dh.com", 500);
+        $funcionario = new Funcionario("Jane", "Doe", "jane@dh.com", 500);
         
         $resul= $funcionario->pagar();
         
         $this->assertTrue(is_string($resul), "O valor de retorno do método pagar deve ser uma string");
         
-        $this->assertTrue(strtolower($resul) === "Jane Doe depositou R$500", "Era esperado 'Jane Doe depositou R$500' e foi recebido '$resul'");
+        $this->assertTrue($resul === "Jane Doe depositou R$500", "Era esperado 'Jane Doe depositou R$500' e foi recebido '$resul'");
     }
